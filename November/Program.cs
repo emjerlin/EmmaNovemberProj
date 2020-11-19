1efad8412e16fed5cf4fce399e77615e4113ee6a
emjerlin <60339583+emjerlin@users.noreply.github.com>
2020-11-19 15:33:29 +0100

    Start

---
+++ November/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace November
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // snake, story spel, fyra i rad mot ai (eller välja spela 2 / ai)
+
+            
+
+        }
+    }
+}
